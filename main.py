7d8e8229bcaf9bd52ac16c737e3929750187c517
babyBear1202 <liyilin1214@163.com>
2021-02-22 11:07:42 +0800

    main.py utils.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import pickle as pk
 import numpy as np
@@ -6,37 +7,56 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import time
+import copy
+import pandas as pd
 import random
+import torch.utils.data as Data
 from stgcn import STGCN
-from utils import generate_dataset, load_metr_la_data, get_normalized_adj
+from utils import DianDataset,get_normalized_adj,load_metr_la_data
+import logging
 
+# logging config
+LOG_FILE = 'train.log'
+logger = logging.getLogger(__file__)
+FORMAT = '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+format_str = logging.Formatter(FORMAT)  # 设置日志格式
+logger.setLevel(logging.DEBUG)  # 设置日志级别
+sh = logging.StreamHandler(stream=sys.stdout)  # 往屏幕上输出
+fh = logging.FileHandler(filename=f'LOG/{LOG_FILE}', mode='w', encoding='utf-8')
+sh.setFormatter(format_str)  # 设置屏幕上显示的格式
+fh.setFormatter(format_str)
+logger.addHandler(sh)  # 把对象加到logger里
+logger.addHandler(fh)
+
+# set random seed
 seed = 42
 np.random.seed(seed)
 torch.manual_seed(seed)
 random.seed(seed)
+
+# training config
 use_gpu = True
 num_timesteps_input = 50
 num_timesteps_output = 10
+epochs = 1000
+batch_size = 128
+feature_num = 5
 
-# epochs = 1000
-epochs=1000
-batch_size = 1000
-
+# handle input argument
 parser = argparse.ArgumentParser(description='STGCN')
 parser.add_argument('--enable-cuda', action='store_true',
                     help='Enable CUDA')
 args = parser.parse_args()
-args.device = None
-# if args.enable_cuda and torch.cuda.is_available():
-#     args.device = torch.device('cuda')
+
 if torch.cuda.is_available():
     args.device = torch.device('cuda')
+    logger.info('Using GPU')
 else:
     args.device = torch.device('cpu')
-print(args.enable_cuda)
-print(torch.cuda.is_available())
+    logger.info('Using CPU')
 
-def train_epoch(training_input, training_target, batch_size):
+
+def train_epoch(training_input, training_target):
     """
     Trains one epoch with the given data.
     :param training_input: Training inputs of shape (num_samples, num_nodes,
@@ -49,42 +69,36 @@ def train_epoch(training_input, training_target, batch_size):
     permutation = torch.randperm(training_input.shape[0])
 
     epoch_training_losses = []
-    epoch_loss_sum = 0
-    epoch_loss_num= 0
     net.train()
-    # print("-----------------------------0---------------------------------")
-    for i in range(0, training_input.shape[0], batch_size):
+    # logger.info("-----------------------------0---------------------------------")
+    # for i in range(0, batch_size):
     # for i in range(10):
 
-        optimizer.zero_grad()
-        # print("-----------------------------1---------------------------------")
-        indices = permutation[i:i + batch_size]
-        X_batch, y_batch = training_input[indices], training_target[indices]
-        current_X_batch = X_batch.to(device=args.device)
-        current_y_batch = y_batch.to(device=args.device)
-        # print("-----------------------------2---------------------------------")
-        out = net(A_wave, current_X_batch)
-        loss = loss_criterion(out, current_y_batch)
-        if i%1000 ==0:
-            print(f'{i}/{training_input.shape[0]}: step loss:{loss.item()}')
-        # print("-----------------------------3---------------------------------")
-        loss.backward()
-        # print("-----------------------------4---------------------------------")
-        optimizer.step()
-        # print("-----------------------------5---------------------------------")
-        # epoch_training_losses.append(loss.clone().detach().cpu().numpy())
-        epoch_loss_sum+=loss.item()
-        epoch_loss_num+=1
-        # print("-----------------------------6---------------------------------")
+    optimizer.zero_grad()
+    logger.info("-----------------------------1---------------------------------")
+    # indices = permutation[i:i + batch_size]
+    X_batch, y_batch = training_input, training_target
+    current_X_batch = X_batch.to(device=args.device)
+    current_y_batch = y_batch.to(device=args.device)
+    # logger.info("-----------------------------2---------------------------------")
+    out = net(A_wave, current_X_batch)
+    loss = loss_criterion(out, current_y_batch)
+        # logger.info(f'{i}/{training_input.shape[0]}: step loss:{loss.item()}')
+        # print(f'{i}/{training_input.shape[0]}: step loss:{loss.item()}')
+    logger.info(f'2--------------------out:{out},current_y_batch:{current_y_batch}')
+    loss.backward()
+    logger.info(f'3--------------------loss:{loss}')
+    # logger.info("-----------------------------4---------------------------------")
+    optimizer.step()
+    logger.info(f'4--------------------loss:{loss}')
+    # logger.info("-----------------------------5---------------------------------")
+    # epoch_training_losses.append(loss.clone().detach().cpu().numpy())
+        # logger.info("-----------------------------6---------------------------------")
     # return sum(epoch_training_losses)/len(epoch_training_losses)
-    return epoch_loss_sum/epoch_loss_num
+    return loss
 
 
-if __name__ == '__main__':
-    torch.manual_seed(7)
-    print(args.device)
-    A, X, means, stds = load_metr_la_data()
-    print(A.shape)
+def split_train_val_test(X, means, stds):
     split_line1 = int(len(X) * 0.6)
     split_line2 = int(len(X) * 0.8)
     #
@@ -96,12 +110,37 @@ if __name__ == '__main__':
 
     test_original_data = X[split_line2:]
     test_mean, test_std = means[split_line2:], stds[split_line2:]
+    return (train_original_data, train_mean, train_std), \
+           (val_original_data, val_mean, val_std ), \
+           (test_original_data, test_mean, test_std)
 
-    training_input, training_target, train_mean_t, train_std_t = generate_dataset(train_original_data,
-                                                       num_timesteps_input=num_timesteps_input,
-                                                       num_timesteps_output=num_timesteps_output,
-                                                       means=train_mean,
-                                                       stds=train_std)
+
+if __name__ == '__main__':
+    logger.info("start loading dataset...")
+    A, X, means, stds = load_metr_la_data()
+    dataset = DianDataset(logger,'C:/From D/电网/电网代码/STGCN-PyTorch-dianwang/np_dataset','train',num_timesteps_input,num_timesteps_output)
+
+
+    loader = Data.DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        drop_last=True,
+        num_workers=0,
+        pin_memory=True)
+    logger.info(f"finish loading dataset, dataset length:{len(dataset)}")
+    # for batch_dix, sample in enumerate(loader):
+        # print(f'sample:[train_input:{sample["train_input"]} train_output:{sample["train_output"]} mean:{sample["mean"].shape}]'
+        # f'std:{sample["std"].shape}')
+        # if batch_dix>=10:
+        #     break
+
+    # (train_original_data, train_mean, train_std), _, _ = split_train_val_test(X, means, stds)
+    #
+    # training_input, training_target, train_mean_t, train_std_t = generate_dataset(train_original_data,
+    #                                                    num_timesteps_input=num_timesteps_input,
+    #                                                    num_timesteps_output=num_timesteps_output,
+    #                                                    means=train_mean,
+    #                                                    stds=train_std)
     # val_input, val_target, val_mean_t, val_std_t = generate_dataset(val_original_data,
     #                                          num_timesteps_input=num_timesteps_input,
     #                                          num_timesteps_output=num_timesteps_output,
@@ -112,34 +151,48 @@ if __name__ == '__main__':
     #                                            num_timesteps_output=num_timesteps_output,
     #                                            means=test_mean,
     #                                            stds=test_std)
-
+    A = pd.read_excel('AdjacencyMatrix.xls').values.astype(np.float32)
     A_wave = get_normalized_adj(A)
     A_wave = torch.from_numpy(A_wave)
-    # A_wave = torch.memory_format(padding)
     A_wave = A_wave.to(device=args.device)
 
     net = STGCN(A_wave.shape[0],
-                training_input.shape[3],
+                feature_num,
                 num_timesteps_input,
                 num_timesteps_output).to(device=args.device)
 
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-5)
     loss_criterion = nn.MSELoss()
-
+    #
     training_losses = []
+    epoch_training_losses = []
     validation_losses = []
     validation_maes = []
     for epoch in range(epochs):
-        loss = train_epoch(training_input, training_target,
-                           batch_size=batch_size)
-        print(f'{epoch}: epoch loss:{loss}')
-        training_losses.append(loss)
-        time.sleep(0.003)
+        for batch_dix, sample in enumerate(loader):
+            #print(f'sample:[train_input:{sample["train_input"].shape} train_output:{sample["train_output"].shape} mean:{sample["mean"].shape}]'
+            #     f'std:{sample["std"].shape}')
+            logger.info(f'idx: {batch_dix}')
+            training_input =torch.from_numpy(np.array(sample["train_input"])).to(device=args.device)
+            training_target = torch.from_numpy(np.array(sample["train_output"])).to(device=args.device)
+            loss = train_epoch(training_input, training_target)
+            logger.info(f'idx: {batch_dix},epoch: {epoch}  loss:{loss}')
+            training_losses.append(loss.clone().detach().cpu().numpy())
+        epoch_training_loss = np.sum(training_losses)/len(training_losses)
+        logger.info(f'{epoch}: epoch loss:{epoch_training_loss}')
+        print(f'{epoch}: epoch loss:{epoch_training_loss}')
+        epoch_training_losses.append(epoch_training_loss)
+        checkpoint_path = "checkpoint"
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
+        file_path = os.path.join('checkpoint', str(epoch))
+        with open(file_path, "wb") as fd:
+            torch.save(net.state_dict(), fd)
 
     torch.save(net, "model/m_xb.pkl")
-    print("Training loss: {}".format(np.stack(training_losses).mean()))
-    plt.plot(training_losses, label="training loss")
+    logger.info("Training loss: {}".format(np.stack(epoch_training_losses).mean()))
+    plt.plot(epoch_training_losses, label="training loss")
     plt.legend()
     plt.show()
     with open("losses_20.pk", "wb") as fd:
-        fd.write(training_losses)
+        fd.write(epoch_training_losses)
--- utils.py
+++ utils.py
@@ -8,6 +8,84 @@ np.set_printoptions(suppress=True)
 import torch
 import pandas as pd
 
+from torch.utils.data.dataset import Dataset
+from typing import Union, Optional
+from pathlib import Path
+import glob
+import os
+import numpy as np
+import logging
+
+from typing import TypeVar
+
+
+
+class DianDataset(torch.utils.data.Dataset):
+
+    def __init__(self, logger: Optional[logging.Logger], dataset_dir: Union[str, Path], dataset_type: str,
+                 num_input_timestep:int, num_predict_timestep:int) -> None:
+        super().__init__()
+        self.logger = logger
+        assert dataset_type in ['train', 'val', 'test']
+        self.npy_data_list = np.array(sorted([os.path.join(dataset_dir, x) for x in glob.glob(dataset_dir+'/*npy')]))
+        self.dataset_dir = dataset_dir
+        self.dataset_type = dataset_type
+        self.num_input_timestep = num_input_timestep
+        self.num_predict_timestep = num_predict_timestep
+        # split train/val/test
+        num_npys = len(self.npy_data_list)
+        # shuffle train/val/test
+        npz_idxes = np.arange(num_npys)
+        np.random.shuffle(npz_idxes)
+        train_split, val_split, test_split = npz_idxes[:int(num_npys*0.6)], npz_idxes[int(num_npys*0.6):int(num_npys*0.8)], \
+                                                npz_idxes[int(num_npys * 0.8):]
+        self.dataset = {'train': self.npy_data_list[train_split],
+                        'val': self.npy_data_list[val_split],
+                        'test': self.npy_data_list[test_split]}[self.dataset_type]
+
+        self.buffer = [None]*len(self.dataset)
+        self.num_npys = len(self.dataset)
+        self.num_samples_each_npy = self.cal_total_samples(self.get_npy(0)[0])
+
+    def get_npy(self, np_index):
+        if self.buffer[np_index] is not None:
+            x = self.buffer[np_index]
+        else:
+            npy_path = self.dataset[np_index]
+            x = np.load(npy_path).transpose((0, 2, 1)).astype(np.float32)
+            self.buffer[np_index] = x
+        mean = np.mean(x, axis=(0, 2))
+        std = np.std(x, axis=(0, 2))
+        import copy
+        return copy.deepcopy(x), mean, std
+
+    def generate_train_sample(self,X, sample_idx):
+        train_input = X[:, sample_idx:sample_idx+self.num_input_timestep,:]
+        train_output = X[:, sample_idx+self.num_input_timestep:sample_idx+self.num_input_timestep+self.num_predict_timestep,0]
+        return torch.from_numpy(train_input), torch.from_numpy(train_output)
+
+    def cal_total_samples(self, x) -> int:
+         return x.shape[1] - self.num_input_timestep-self.num_predict_timestep+1
+
+    def __len__(self):
+        return self.num_npys * self.num_samples_each_npy
+
+    def __getitem__(self, index):
+
+        if self.logger:
+            self.logger.debug(f'current sample:{index}')
+        np_index, sample_index = index//self.num_samples_each_npy, index%self.num_samples_each_npy
+        x, mean, std = self.get_npy(np_index)
+        train_input, train_output = self.generate_train_sample(x, sample_index)
+        sample = {
+            'train_input': train_input,
+            'train_output': train_output,
+            'mean': mean,
+            'std': std}
+        # if index == 1916: print(sample)
+        return sample
+
+
 
 def load_metr_la_data():
     # if (not os.path.isfile("data/adj_mat.npy")
